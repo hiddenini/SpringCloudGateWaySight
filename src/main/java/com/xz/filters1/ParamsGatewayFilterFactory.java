df368be861e09ad0ca70826169255b3d098c3a51
zjw <1300151235@qq.com>
2020-06-17 18:30:24 +0800

    dynamic route

---
+++ src/main/java/com/xz/filters1/ParamsGatewayFilterFactory.java
@@ -0,0 +1,34 @@
+package com.xz.filters1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+@Component
+public class ParamsGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> implements Ordered {
+    @Override
+    public GatewayFilter apply(Object config) {
+        return ((exchange, chain) -> {
+            MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
+            String sessionTokenId = queryParams.getFirst("merchantId");
+            if (StringUtils.isEmpty(sessionTokenId)) {
+                log.info("merchantId为空,非法请求");
+                exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+                return exchange.getResponse().setComplete();
+            }
+            return chain.filter(exchange);
+        });
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
